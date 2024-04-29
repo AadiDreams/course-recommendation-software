fe1ecf7c7bf53ef66693de6234b04733f1633220
Aadithya V Anand <aadithyadreams@gmail.com>
2024-04-29 23:39:39 +0530

    firebase script added in reg.js

---
+++ reg.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyAGnHliNxthPiirMurAyoo23mJsb7GOYD0",
+    authDomain: "career-craft-96422.firebaseapp.com",
+    projectId: "career-craft-96422",
+    storageBucket: "career-craft-96422.appspot.com",
+    messagingSenderId: "562722312371",
+    appId: "1:562722312371:web:4504674defbe7d02ace0c6"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);

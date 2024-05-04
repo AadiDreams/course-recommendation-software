94255096875f73a4d78b4a6933283c5d79035e00
Aadithya V Anand <aadithyadreams@gmail.com>
2024-05-05 01:56:29 +0530

    server update

--- server.js
+++ server.js
@@ -7,13 +7,14 @@ const db = knex({
   client: 'pg',
   connection: {
     host: '127.0.0.1',
-    user: 'postgres',
-    password: 'postgres',
-    database: 'CareerCraft'
+    user: 'ccadmin',
+    password: 'ccadmin',
+    database: 'careercraft'
   }
 })
 
 const app = express();
+const port = 3000;
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname));
@@ -22,6 +23,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 })
 
-app.listen(3000, (req, res) => {
-  console.log('listening on port 3000......')
+app.get('/login.html', (req, res) => {
+  res.sentFile(path.join(__dirname),"about.html");
+})
+
+app.listen(port, (req, res) => {
+  console.log(`Server started on port ${port}`);
 })

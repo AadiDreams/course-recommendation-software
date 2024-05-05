87818f883ba8f54ef39392913c792d750cdc4574
Aadithya V Anand <aadithyadreams@gmail.com>
2024-05-05 21:11:08 +0530

    update

--- database.js
+++ database.js
@@ -1,28 +1,28 @@
-const { Client } = require('pg');
+// const { Client } = require('pg');
 
-const client = new Client({
-    host: "localhost",
-    port: 5432,
-    user: "ccadmin",
-    password: "ccadmin",
-    database: "careercraft"
-})
+// const client = new Client({
+//     host: "localhost",
+//     port: 5432,
+//     user: "ccadmin",
+//     password: "ccadmin",
+//     database: "careercraft"
+// })
 
-client.connect();
+// client.connect();
 
-client.on("connect", () => {
-    console.log("Database connection");
-})
-client.on("end", () => {
-    console.log("Connection end")
-})
+// client.on("connect", () => {
+//     console.log("Database connection");
+// })
+// client.on("end", () => {
+//     console.log("Connection end")
+// })
 
-client.query(`select name from users where id=1;`,(err, result) => {
-    if(!err) {
-        console.log(result.rows);
-    }
-    else{
-        console.log(err);
-    }
-    client.end();
-})
+// client.query(`select name from users where id=1;`,(err, result) => {
+//     if(!err) {
+//         console.log(result.rows);
+//     }
+//     else{
+//         console.log(err);
+//     }
+//     client.end();
+// })
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const knex = require('knex');
+const pg = require('pg');
+const ejs = require('ejs');
 
-const db = knex({
-  client: 'pg',
-  connection: {
-    host: '127.0.0.1',
-    user: 'ccadmin',
-    password: 'ccadmin',
-    database: 'careercraft'
-  }
-})
+const pool = new pg.Pool({
+  user: 'ccadmin',
+  password: 'ccadmin',
+  database: 'careercraft',
+  host: 'localhost',
+  port: 5432
+});
+
 
 const app = express();
 const port = 3000;
@@ -23,9 +23,19 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 })
 
-app.get('/login.html', (req, res) => {
-  res.sentFile(path.join(__dirname),"about.html");
-})
+app.get('/qa', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT question FROM qs');
+    const questions = result.rows.map(row => row.question);
+    const randomQuestion = questions[Math.floor(Math.random() * questions.length)];
+    await client.release();
+    res.render('qa', { question: randomQuestion });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 app.listen(port, (req, res) => {
   console.log(`Server started on port ${port}`);

a725d5c90d0b255d54ffbe116a17b2a73e60adef
Aadithya V Anand <aadithyadreams@gmail.com>
2024-05-06 01:32:58 +0530

    working draft

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const path = require('path');
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json()); // Parse JSON bodies
+const bodyParser = require('body-parser');
+
+// Middleware to parse JSON request bodies
+app.use(bodyParser.json());
 
 // Routes
 app.get('/', (req, res) => {
@@ -25,7 +30,7 @@ const pool = new Pool({
 });
 
 // Example query
-pool.query('SELECT * FROM qs', (err, result) => {
+pool.query('SELECT count(*) FROM qs', (err, result) => {
     if (err) {
         return console.error('Error executing query', err.stack);
     }
@@ -37,6 +42,30 @@ pool.query('SELECT * FROM qs', (err, result) => {
 // Serve index.html when the server starts
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
+  console.log("index page successfull");
+});
+
+app.get('/signIn', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/login.html'));
+  console.log("signIn page successfull");
+});
+
+
+// Handle login POST request
+app.post('/login', (req, res) => {
+  // Extract username and password from request body
+  console.log(req.body);
+  const { username, password } = req.body;
+  console.log(res);
+  // Here you can validate the username and password
+  // For now, let's just send a response based on whether they are provided or not
+  if (username && password) {
+      res.status(200).send(`Received username: ${username}, password: ${password}`);
+  } else {
+      console.log(req.body);
+      res.status(400).send(`Username, password not wokring: ${username}, password: ${password}`);
+
+  }
 });
 
 // Start the server

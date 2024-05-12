32a88a6e5b3e529a5c2b9f4a317735a42bb53fd5
Aadithya V Anand <aadithyadreams@gmail.com>
2024-05-12 15:17:28 +0530

    links changed to server based

--- server.js
+++ server.js
@@ -42,35 +42,48 @@ pool.query('SELECT count(*) FROM qs', (err, result) => {
 // Serve index.html when the server starts
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
-  console.log("index page successfull");
+  console.log("index page successful");
 });
 
 app.get('/signIn', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/login.html'));
-  console.log("signIn page successfull");
+  console.log("signIn page successful");
 });
 
 app.get('/register', (req, res) => {
   res.sendFile(path.join(__dirname, "public/register.html"));
-  console.log("register page successfull");
+  console.log("register page successful");
 });
 
-// Handle login POST request
-app.post('/login', (req, res) => {
-  // Extract username and password from request body
-  console.log(req.body);
-  const { username, password } = req.body;
-  console.log(res);
-  // Here you can validate the username and password
-  // For now, let's just send a response based on whether they are provided or not
-  if (username && password) {
-      res.status(200).send(`Received username: ${username}, password: ${password}`);
-  } else {
-      console.log(req.body);
-      res.status(400).send(`Username, password not wokring: ${username}, password: ${password}`);
-
-  }
+app.get('/logout', (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+  console.log("logout successful");
 });
+
+app.get('/questions', (req, res) => {
+  res.sendFile(path.join(__dirname, "public/questions.html"));
+});
+
+app.get('/userHome', (req, res) => {
+  res.sendFile(path.join(__dirname, "public/signed.html"));
+});
+
+// // Handle login POST request
+// app.post('/login', (req, res) => {
+//   // Extract username and password from request body
+//   console.log(req.body);
+//   const { username, password } = req.body;
+//   console.log(res);
+//   // Here you can validate the username and password
+//   // For now, let's just send a response based on whether they are provided or not
+//   if (username && password) {
+//       res.status(200).send(`Received username: ${username}, password: ${password}`);
+//   } else {
+//       console.log(req.body);
+//       res.status(400).send(`Username, password not wokring: ${username}, password: ${password}`);
+
+//   }
+// });
 
 // Start the server
 app.listen(PORT, () => {
